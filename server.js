5925c1e8c9716bfb6b42756a457912a35dbc58cd
okidijimmy200 <okidijimmie@gmail.com>
2020-05-28 15:50:37 +0300

    added mongoose

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// importing the mongoose
+const mongoose = require('mongoose')
 // importing dotenv file
 const dotenv = require('dotenv')
 
@@ -6,6 +8,24 @@ const app = require('./app');
 
 // uing the dotenv variable
 dotenv.config({path: './config.env'})
+
+// getting our database password
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+//////////connecting to local database/////////////////////
+// mongoose.connect(process.env.DATABASE_LOCAL,{
+//     // objects to deal with warnings
+//     useNewUrlParser:true,
+//     useCreateIndex:true,
+//     useFindAndModify:false
+// }).then(() => console.log('DB connection successful'));
+
+// mongoose remote
+mongoose.connect(DB, {
+    // objects to deal with warnings
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+}).then(() => console.log('DB connection successful'));
 
 // to check the environment we are currently in
 // console.log(app.get('env'))
