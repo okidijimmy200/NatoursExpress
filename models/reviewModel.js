675c75f4b97968a8e0717d6a95073ecc26c2bf30
okidijimmy200 <okidijimmie@gmail.com>
2020-08-07 14:15:42 +0300

    Modelling Reviews Parent Referencing:

---
+++ models/reviewModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const reviewSchema = new mongoose.Schema({
+    review: {
+        type: String,
+        required:[true, 'Review can not be empty']
+    },
+    rating:{
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    // tour parent referencing
+    tour: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true,'Review must belong to a tour']
+    },
+    // user parent referencing
+    user:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Review must belong to a user']
+    }
+},
+// --object for options
+// incase we have a field thtis not stored in the db bt calculated using some other value, we want it to show up when there is an outpu
+{
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true}
+    // --we can now see the duration in weeks
+}
+)
+
+const Review = mongoose.model('Review', reviewSchema)
+
+
+module.exports = Review
+
+//review needs tobelongto a tour and also has an author,
+// --parent referencing here bse both tour and user are the parents of the data set
+// --the reviews section will have multiple reviews and bse we dont knw how long our arrays
+// willgrow, its best we use parent referencing
