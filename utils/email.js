816b8b01c2b4b80da41da573b86efc5496e530f6
okidijimmy200 <okidijimmie@gmail.com>
2020-08-26 14:31:56 +0300

    Building acomplex Email Handler

--- utils/email.js
+++ utils/email.js
@@ -1,28 +1,57 @@
 const nodemailer = require('nodemailer')
+const pug = require('pug')
+const htmlTotext = require('html-to-text')
 
-const sendEmail = async options => {
-    // email address, subject line, email content
-    // 1) Create a transporter ie gmail
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,  // servie
-        port: process.env.EMAIL_PORT,
-        auth: { // auth properties wch  is stored in the config.env file
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }  
-        // activate in gmail the "less secure app options"(gmail is not gd for prod app)
-    })
-    // 2) Define the email options
-    const mailOptions = {
-        // specify where the email is coming from
-        from: 'Okidi Jimmy <hello@jimmyjones.io>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-        // html:
+// --class for email
+module.exports = class Email {
+    constructor(user,url) {
+        this.to = user.email;
+        this.firstname = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `Okidi Jimmy <${process.env.EMAIL_FROM}>`
     }
-    // 3) Send the email with nodemailer
-    await transporter.sendMail(mailOptions)
+
+    newTransport() {
+        if(process.env.NODE_ENV === 'production') {
+            // sender id
+            return 1
+        }
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,  // servie
+            port: process.env.EMAIL_PORT,
+            auth: { // auth properties wch  is stored in the config.env file
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }  
+            // activate in gmail the "less secure app options"(gmail is not gd for prod app)
+        })
+    }
+    // --send method to send email
+    async send(template, subject) {
+        // 1) render html based on a pug template
+        const html =  pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+            firstname: this.firstname,
+            url: this.url,
+            subject
+
+        })
+
+        // 2) define the email options
+        const mailOptions = {
+                    // specify where the email is coming from
+                    from: this.from,
+                    to: this.to,
+                    subject,
+                    text: htmlTotext.fromString(html), //convert html to text
+                    html
+                }
+        // 3) create a transport and create an email
+        await this.newTransport().sendMail(mailOptions)
+
+    }
+    async sendWelcome() {
+        await this.send('welcome', 'Welcome to the natours family')
+    }
 }
 
-module.exports = sendEmail;
+
